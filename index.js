3353d5f430713b90f4784a50f91db7e59e31debb
Alejandro Arrebola <aarrebola16@gmail.com>
2024-05-03 13:52:28 +0200

    my firts API

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy as fck",
+    important: false,
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: false,
+  },
+  {
+    id: "4",
+    content: "asdasd",
+    important: false,
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello world</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter((note) => note.id !== id);
+  response.status(204).end();
+});
+
+app.post("/api/notes", (request, response) => {
+  const note = request.body;
+  //console.log("la nota:", note);
+  const ids = notes.map((note) => note.id);
+  const maxId = Math.max(...ids);
+
+  if (!note || !note.content) {
+    return response.status(400).json({
+      error: "Note.content is missing",
+    });
+  }
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    important: typeof note.important !== "undefined" ? note.important : false,
+  };
+
+  notes = notes.concat(newNote);
+  response.json(newNote);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
